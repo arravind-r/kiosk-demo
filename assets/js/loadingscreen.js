c0ce8ee725ce38e909dda5aff1860a457cf8c75b
Arravind <aravindram024@gmail.com>
2024-01-17 21:03:55 +0530

    added new player + singles slide  - part 2

---
+++ assets/js/loadingscreen.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function () {
+	const loadingScreen = document.getElementById("loading-screen");
+	setTimeout(function () {
+		loadingScreen.style.display = "none";
+	}, 800);
+});
